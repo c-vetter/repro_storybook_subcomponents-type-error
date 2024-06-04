b0613c783c608554db6bda0cb1354216fa7cae2c
Christian Vetter <hallo@cv-o.de>
2024-06-04 16:33:50 +0200

    npx storybook@latest upgrade
    applied migrations:
    + react-docgen
    + webpack5-compiler-setup
    + upgradeStorybookRelatedDependencies
    + autodocs-tags

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,16 @@
 export default {
   stories: ["../src/**/*.stories.(ts|tsx|js|jsx)"],
+
   framework: {
     name: "@storybook/react-webpack5",
     options: {},
   },
-  addons: ["@storybook/addon-essentials"],
-  docs: {
-    autodocs: true,
-  },
+
+  addons: ["@storybook/addon-essentials", "@storybook/addon-webpack5-compiler-swc"],
+
+  docs: {},
+
+  typescript: {
+    reactDocgen: "react-docgen-typescript"
+  }
 };
